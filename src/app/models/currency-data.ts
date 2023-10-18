e7b53da2def7201eab3923fe7d9e9a12c57ee0f1
Hirota <stefanohirota@gmail.com>
2023-10-18 10:11:36 -0300

    feat: criacao da interface currency data

---
+++ src/app/models/currency-data.ts
@@ -0,0 +1,13 @@
+export interface CurrencyData {
+  code: string;
+  codein: string;
+  name: string;
+  high: number;
+  low: number;
+  varBid: number;
+  pctChange: number;
+  bid: number;
+  ask: number;
+  timestamp: number;
+  create_date: string;
+}
